a5a62386e173933a0372707a93e0419ecb254b56
UndERcaVEN <9189759110z@gmail.com>
2022-06-23 17:11:58 +0300

    adaptive changes

--- Events/main.js
+++ Events/main.js
@@ -3,6 +3,9 @@ let header=document.querySelector("header")
 let navbar=document.querySelector(".navbar")
 let client_height=document.documentElement.clientHeight
 let exp_list=document.querySelector(".expanded_list")
+let phone_options_list=document.querySelectorAll(".phone_options_expanding")
+let burger=document.querySelector(".burger")
+let phone_navbar=document.querySelector(".phone_navbar")
 
 console.log(header)
 
@@ -10,11 +13,11 @@ document.body.onwheel=function(event){
 
     if(window.scrollY>100){
         navbar.style.backgroundColor="rgba(54, 53, 49,1)"
-        // exp_list.style.backgroundColor="rgba(54, 53, 49,1)"
+        
     }
     else{
         navbar.style.backgroundColor="rgba(54,53,49,0.5)"
-        // exp_list.style.backgroundColor="rgba(54,53,49,0.5)"
+        
     }
     if(event.deltaY>0 && window.scrollY>100){
         navbar.style.top="-63px"
@@ -23,4 +26,14 @@ document.body.onwheel=function(event){
         navbar.style.top="0"
     }
     console.log(event)
+}
+
+phone_options_list.forEach(element => {
+    element.onclick=function(event){
+        element.children[1].children[0].classList.toggle("invisible")
+    }
+})
+
+burger.onclick=function(event){
+    phone_navbar.classList.toggle("invisible")
 }
