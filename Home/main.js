7be292578e6f3c7b08554fbe6993c28db988bf91
feroswin <vitalkaivanov009@mail.ru>
2022-06-24 14:02:55 +0300

    adaptive section about and solutions in main page

--- Home/main.js
+++ Home/main.js
@@ -57,21 +57,6 @@ achievements_btn.onclick = () => {
   },200)
 }
 
-// SECTION SOLUTIONS 
-
-$(document).ready(function () {
-
-  $('.solutions__card__inner').mouseleave(function () { 
-    this.style.marginTop = ''
-    this.style.boxShadow = ''
-  });
-
-  $('.solutions__card__inner').mouseenter(function () { 
-    this.style.marginTop = '20px'
-    this.style.boxShadow = '0px 0px 40px 0px #40a4dd'
-  });
-});
-
 // WEBINARS scroll
 const webinars = document.querySelector(".webinar-list")
 const webinarsCount = webinars.childElementCount
